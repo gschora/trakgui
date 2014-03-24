7c866d18e164fa3c77d04bba3fbc3416fed1fe5a
gschora <hannes.dummy@putz-transporte.at>
2014-03-24 18:13:39 +0100

    startscript for rtkrcv config for rtkrcv

---
+++ tools/rtklib/startrtkrcv.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "starting rtkrcv..."
+
+/usr/bin/screen -S rtkrcv -d -m sh /home/hannes/projects/2014/myGitProjects/trakgui/tools/rtklib/rtkrcv -s -p 20200 -m 20300 -o pflanzen2014.conf
+
+exit 0
