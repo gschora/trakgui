7cd08c0f197442a5872dd15d1feddab709bfc3dd
gschora <hannes.dummy@putz-transporte.at>
2014-03-24 18:54:24 +0100

    fixed start problems of rtkrcv

--- tools/rtklib/startrtkrcv.sh
+++ tools/rtklib/startrtkrcv.sh
@@ -2,6 +2,6 @@
 
 echo "starting rtkrcv..."
 
-/usr/bin/screen -S rtkrcv -d -m sh /home/hannes/projects/2014/myGitProjects/trakgui/tools/rtklib/rtkrcv -s -p 20200 -m 20300 -o pflanzen2014.conf
+/usr/bin/screen -S rtkrcv -d -m /home/hannes/projects/2014/myGitProjects/trakgui/tools/rtklib/rtkrcv -s -p 20200 -m 20300 -o pflanzen2014.conf
 
 exit 0
