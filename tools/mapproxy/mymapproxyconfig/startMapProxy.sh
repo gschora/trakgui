b08fd18eb0e715d88dd30799969b1fb0d8d86a37
gschora <hannes.dummy@putz-transporte.at>
2014-03-25 00:16:49 +0100

    removed files

--- tools/mapproxy/mymapproxyconfig/startMapProxy.sh
+++ tools/mapproxy/mymapproxyconfig/startMapProxy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-../bin/mapproxy-util serve-develop mymapproxyConfig.yaml -b 0.0.0.0:8080
+/usr/bin/screen -S mapproxy -d -m ../bin/mapproxy-util serve-develop mymapproxyConfig.yaml -b 0.0.0.0:8080
